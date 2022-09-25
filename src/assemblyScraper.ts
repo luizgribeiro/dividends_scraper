d28988e1ec0539ab80c630e42045631680f126e8
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-09-25 05:56:22 -0300

    feat: initial scrape done

--- src/assemblyScraper.ts
+++ src/assemblyScraper.ts
@@ -17,12 +17,14 @@ import fs from "fs";
         
         type dividend = {
             company: string;
-            date: string;
+            approvalDate: string;
             type: string;
             exDate: string;
             paymentDate: string;
-            value: string
+            value: string[]
         }
+        
+        const dividends: Array<dividend> = [];
         
         tables.each( (idx, el)=> {
             const values: Array<string> = []; 
@@ -34,63 +36,24 @@ import fs from "fs";
                 case 7:
                 case 9:
                 case 11:
+                case 12:
                     values.push($(el2).text())
                     break;
-                case 12:
-                    console.log(" +++++++++++++++", $(el).text())
                } 
             })
-            console.log(values) 
-        })
+            dividends.push({
+                company: values[0],
+                approvalDate: values[1],
+                type: values[2],
+                exDate: values[3],
+                paymentDate: values[4],
+                value: values.slice(5)
 
-        //$(tables).children("tr")//.each((i, el: any)=> {console.log("oi")})
-        /*
-        const rows = $(tableBody).children("tr")
-        const summarized: Array<any> = [];
-        const rowData: Array<any> = [];
-        rows.each((i, element)=> {
-                const rd: Array<any> = []
-                $(element).children("td").each((i, el: any)=> {rd.push($.text(el.children))})
-                rowData.push(rd)
+            })
         })
         
-        const headers = "data_com,valor_abrev,data,tipo,valor_full";
-        console.log(rowData)
-        
-        for (let i=0; i < rowData.length; i++ ) {
-            const occurrences: Array<any>= [rowData[i]]; 
-            const refDate = rowData[i][0]
-            for(let j=i+1; j < rowData.length; j++) {
-                const compDate = rowData[j][0];
-                if (refDate === compDate) {
-                    occurrences.push(rowData[j])
-                }
-            }
-            console.log(occurrences) 
-            
-            if (occurrences.length === 1) {
-                summarized.push([
-                    occurrences[0][0],
-                    occurrences[0][2],
-                    occurrences[0][3],
-                    parseFloat(occurrences[0][4])
-                ])
-            } else {
-                const sum = occurrences.map(occ=> parseFloat(occ[4])).reduce((prev, curr)=> prev + curr, 0);
-                summarized.push([
-                    occurrences[0][0],
-                    occurrences[0][2],
-                    "sum",
-                    sum
-                ])
-            }
-        } 
-        console.log(summarized)
-        //const csvRows = rowData.map(row=> row.map((data: any) => data.replace(',', '.')).join(","))
-        
-        //console.log(csvRows)
-        */
+        console.log(dividends)
 
-    }
+           }
 
 })()
