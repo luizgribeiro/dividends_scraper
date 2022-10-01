bc49b1f3439660981c421373ba76f8bc5ea0ed4f
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-10-01 17:35:03 -0300

    refactor: segregate implementation from tests

---
+++ src/transforms.ts
@@ -0,0 +1,51 @@
+import type { Dividend } from "../src/scraper";
+
+export const parseDividends = (dividends: Dividend[]) => {
+  return dividends.map((div) => ({
+    ...parseCompany(div.company),
+    type: "Dividendo",
+    values: parseValues(div.value),
+    approvalDate: parseDate(div.approvalDate),
+    exDate: parseDate(div.exDate),
+    paymentDate: parseDate(div.paymentDate),
+  }));
+};
+
+export const parseValues = (values: string[]) => {
+  const splitted = values.map((v) => v.split(" "));
+
+  return splitted.map((v) => {
+    return {
+      amount: parseFloat(v[1].replace(",", ".")),
+      type: v[5],
+    };
+  });
+};
+
+export const parseCompany = (company: string) => {
+  const trimmedCompany = company.trim();
+  const [ticker, companyName] = trimmedCompany.split(" - ");
+  return { ticker, companyName };
+};
+
+export const parseDate = (localStrDate: string) => {
+  const [_, meaningFullDate] = localStrDate.split(", ");
+  const [day, month, year] = meaningFullDate.split(" de ");
+
+  const monthIdx = {
+    Janeiro: 0,
+    Fevereiro: 1,
+    Março: 2,
+    Abril: 3,
+    Maio: 4,
+    Junho: 5,
+    Julho: 6,
+    Agosto: 7,
+    Setembro: 8,
+    Outubro: 9,
+    Novembro: 10,
+    Dezembro: 11,
+  };
+
+  return new Date(parseInt(year), monthIdx[month], parseInt(day));
+};
--- tests/transforms.spec.ts
+++ tests/transforms.spec.ts
@@ -1,3 +1,10 @@
+import {
+  parseCompany,
+  parseDividends,
+  parseDate,
+  parseValues,
+} from "../src/transforms";
+
 describe("Data transforms made on dividends info array", () => {
   const DividendSample = {
     company: "ITUB - itau unibanco holding s.a.   \n",
@@ -9,33 +16,6 @@ describe("Data transforms made on dividends info array", () => {
       "R$ 0,015 por ação ITAUUNIBANCO ON N1",
       "R$ 0,015 por ação ITAUUNIBANCO PN N1",
     ],
-  };
-  const parseCompany = (company: string) => {
-    const trimmedCompany = company.trim();
-    const [ticker, companyName] = trimmedCompany.split(" - ");
-    return { ticker, companyName };
-  };
-
-  const parseDate = (localStrDate: string) => {
-    const [_, meaningFullDate] = localStrDate.split(", ");
-    const [day, month, year] = meaningFullDate.split(" de ");
-
-    const monthIdx = {
-      Janeiro: 0,
-      Fevereiro: 1,
-      Março: 2,
-      Abril: 3,
-      Maio: 4,
-      Junho: 5,
-      Julho: 6,
-      Agosto: 7,
-      Setembro: 8,
-      Outubro: 9,
-      Novembro: 10,
-      Dezembro: 11,
-    };
-
-    return new Date(parseInt(year), monthIdx[month], parseInt(day));
   };
   describe("parseCompany", () => {
     it("Should return only the company ticker", () => {
@@ -55,16 +35,6 @@ describe("Data transforms made on dividends info array", () => {
       );
     });
   });
-  const parseValues = (values: string[]) => {
-    const splitted = values.map((v) => v.split(" "));
-
-    return splitted.map((v) => {
-      return {
-        amount: parseFloat(v[1].replace(",", ".")),
-        type: v[5],
-      };
-    });
-  };
   describe("parseValues", () => {
     it("Should return an array with amount and type for the provided array of values", () => {
       expect(parseValues(DividendSample.value)).toEqual([
@@ -74,16 +44,6 @@ describe("Data transforms made on dividends info array", () => {
     });
   });
 
-  const parseDividends = (dividends: Dividend[]) => {
-    return dividends.map((div) => ({
-      ...parseCompany(div.company),
-      type: "Dividendo",
-      values: parseValues(div.value),
-      approvalDate: parseDate(div.approvalDate),
-      exDate: parseDate(div.exDate),
-      paymentDate: parseDate(div.paymentDate),
-    }));
-  };
   describe("parseDividends", () => {
     it("Should return an array with dividends objects with all it's fields parsed", () => {
       expect(parseDividends([DividendSample])).toEqual([
