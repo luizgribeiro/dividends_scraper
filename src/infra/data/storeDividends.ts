77fbf286c6ffc3efaee49d5cca36e974fb66fd76
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-10-22 11:59:19 -0300

    handle multiple storage failures and return only successful ones

--- src/infra/data/storeDividends.ts
+++ src/infra/data/storeDividends.ts
@@ -3,25 +3,23 @@ import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { ParsedDividends } from "@/transforms";
 
 export const storeDividends = async (dividends: ParsedDividends[]) => {
-  try {
-    const storageResult = await Promise.all(
-      dividends.map((div) =>
-        ddbDocClient.send(
-          new PutCommand({
-            TableName: "dividends-table-dev",
-            Item: {
-              ...div,
-              approvalDate: div.approvalDate.getTime(),
-            },
-            ConditionExpression:
-              "attribute_not_exists(approvalDate) AND attribute_not_exists(ticker)",
-          })
-        )
+  const storageResult = await Promise.allSettled(
+    dividends.map((div) =>
+      ddbDocClient.send(
+        new PutCommand({
+          TableName: "dividends-table-dev",
+          Item: {
+            ...div,
+            approvalDate: div.approvalDate.getTime(),
+          },
+          ConditionExpression:
+            "attribute_not_exists(approvalDate) AND attribute_not_exists(ticker)",
+        })
       )
-    );
-    return storageResult;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+    )
+  );
+
+  return dividends
+    .map((div, idx) => (storageResult[idx].status === "rejected" ? null : div))
+    .filter((result) => (result === null ? false : true));
 };
